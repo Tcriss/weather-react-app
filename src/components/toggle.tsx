5cfe193bb3991a202e3e85d6f92edcee67d8d946
Cristian <do.cristian.tr@hotmail.com>
2024-05-06 00:03:01 -0400

    style: minor UI changes

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useEffect, useRef, useState } from "react";
 import { AnimatePresence } from 'framer-motion';
 
-function Toggle({ children, icon }: { children: ReactNode, icon: string }) {
+function Toggle({ children, icon, activeIcon, color }: { children: ReactNode, icon: string, activeIcon: string, color: string }) {
     const [isOpen, setOpen] = useState<boolean>(false);
     const menuRef = useRef<any>();
 
@@ -18,8 +18,8 @@ function Toggle({ children, icon }: { children: ReactNode, icon: string }) {
     return (
         <section ref={menuRef}>
             <div id="toggle" className="w-7 h-full flex justify-center items-center">
-                <button onClick={() => setOpen(val => val = !val)} className="w-full h-7 flex rounded-full justify-center items-center transition-all bg-slate-50/25 hover:bg-yellow-500 hover:text-white hover:5 active: active:shadow-lg">
-                    <i className={`fi ${icon} flex justify-center items-center`}></i>
+                <button onClick={() => setOpen(val => val = !val)} className={`w-full h-7 flex rounded-full justify-center items-center transition-all ${ isOpen ? color : 'bg-slate-50/25'} hover:${color} hover:text-white hover:5 active: active:shadow-lg`}>
+                    <i className={`fi ${ isOpen ? activeIcon : icon } flex justify-center items-center`}></i>
                 </button>
             </div>
             <AnimatePresence>{ isOpen && children }</AnimatePresence>
--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -9,13 +9,21 @@ function Toolbar() {
         <nav id="toolbar" className="w-full flex justify-between gap-2 px-3 py-3 md:px-10 transition-all">
             <div className="xs:w-0 md:w-16"></div>
             <div>
-                <Search/>
+                <Search />
             </div>
             <div className="flex gap-3">
-                <Toggle icon={'fi-br-star'}>
+                <Toggle
+                    icon={'fi-br-star'}
+                    activeIcon={'fi-sr-star'}
+                    color={'bg-yellow-500'}
+                >
                     <FavoritesMenu></FavoritesMenu>
                 </Toggle>
-                <Toggle icon={'fi-br-settings'}>
+                <Toggle
+                    icon={'fi-br-settings'}
+                    activeIcon={'fi-sr-settings'}
+                    color={'bg-blue-500'}
+                >
                     <SettingsMenu></SettingsMenu>
                 </Toggle>
             </div>
